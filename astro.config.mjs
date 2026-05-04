4489444a079ab7a73cbfc04b97317d5b5adeaa7a
Lukas Pscheidt <social.predik@gmail.com>
2026-05-04 20:03:53 +0200

    feat(seo): přidáno generování sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+
   // site: "https://revimont-klatovy.cz",
   site: "https://revimont.vercel.app/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
